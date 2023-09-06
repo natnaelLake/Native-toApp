cde60d6950b1c48623c266d39a44c352677e1438
bewuketulake <bewuketulake1212@gmail.com>
2023-09-06 21:02:46 +0300

    intialze the first commit

--- App.js
+++ App.js
@@ -1,11 +1,56 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  TextInput,
+  FlatList
+} from "react-native";
 
 export default function App() {
+  const [person, setPerson] = useState([
+    {
+      name: "abebe",
+      key: "1"
+    },
+    {
+      name: "kebede",
+      key: "2"
+    },
+    {
+      name: "aster",
+      key: "3"
+    },
+    {
+      name: "belete",
+      key: "4"
+    },
+    {
+      name: "dubale",
+      key: "5"
+    },
+    {
+      name: "alemu",
+      key: "6"
+    }
+  ]);
+  console.log(person);
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <FlatList
+        numColumns={2}
+        data={person}
+        renderItem={({ item }) => <Text style={styles.item}>{item.name}</Text>}
+      />
+      {/* <ScrollView>
+        {person.map((man) => (
+          <View key={man.key}>
+            <Text style={styles.item}>{man.name}</Text>
+          </View>
+        ))}
+      </ScrollView> */}
     </View>
   );
 }
@@ -13,8 +58,16 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    paddingTop: 40,
+    paddingHorizontal: 20
   },
+  item: {
+    marginTop: 25,
+    padding: 30,
+    backgroundColor: "pink",
+    fontSize: 24,
+    marginHorizontal:10,
+    marginTop:24,
+  }
 });
